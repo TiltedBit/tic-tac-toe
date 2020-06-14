6865d8a180fa09371700f55784653b6581e00f45
Gijs de Jong <14833076+OxKitsune@users.noreply.github.com>
2020-06-14 17:37:18 +0200

    Add gamestate

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace Assets.Scripts {
 
-    // Update is called once per frame
-    void Update()
-    {
+    public class GameStateManager : MonoBehaviour {
+
         
+        /// <summary>
+        /// The game's current state
+        /// </summary>
+        public GameState GameState;
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
     }
 }
+
+
+
