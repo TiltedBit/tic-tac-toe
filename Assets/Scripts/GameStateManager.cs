74d5076503905cd8fcc789ff634fe3da1592359d
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2020-06-14 11:49:26 -0400

    Remove unused imports

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,7 +1,4 @@
-﻿using Assets.Scripts;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts {
 
@@ -11,7 +8,7 @@ namespace Assets.Scripts {
         /// <summary>
         /// The game's current state
         /// </summary>
-        public GameState GameState;
+        public GameState gameState;
 
         // Start is called before the first frame update
         void Start() {
